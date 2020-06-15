2ff150f3dfd85e1dff8b403108ec648cd98af7c4
David Montandon <dave.tendon@gmail.com>
2020-06-16 00:38:41 +0200

    Added UI for ItemPedestal

--- src/main/java/com/davidmontandon/yggdrasil/util/ClientEventBusSubscriber.java
+++ src/main/java/com/davidmontandon/yggdrasil/util/ClientEventBusSubscriber.java
@@ -1,13 +1,19 @@
 package com.davidmontandon.yggdrasil.util;
 
 import com.davidmontandon.yggdrasil.Yggdrasil;
+import com.davidmontandon.yggdrasil.client.gui.ItemPedestalScreen;
+import com.davidmontandon.yggdrasil.client.tileentity.render.ItemPedestalRenderer;
 import com.davidmontandon.yggdrasil.init.BlockInit;
+import com.davidmontandon.yggdrasil.init.YggdrasilContainerTypes;
+import com.davidmontandon.yggdrasil.init.YggdrasilTileEntityTypes;
 
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraft.client.gui.ScreenManager;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 
@@ -20,6 +26,10 @@ public class ClientEventBusSubscriber {
 		RenderTypeLookup.setRenderLayer(BlockInit.MUSPELHEIM_EPLUPHIFER_SAPLING.get(), RenderType.getCutout());		
 		RenderTypeLookup.setRenderLayer(BlockInit.MUSPELHEIM_IKADAMIA_CROP.get(), RenderType.getCutout());
 		RenderTypeLookup.setRenderLayer(BlockInit.MUSPELHEIM_ASH_CARAMBOLA_BUSH.get(), RenderType.getCutout());		
+		
+		ScreenManager.registerFactory(YggdrasilContainerTypes.ITEM_PEDESTAL.get(), ItemPedestalScreen::new);
+
+		ClientRegistry.bindTileEntityRenderer(YggdrasilTileEntityTypes.ITEM_PEDESTAL.get(), ItemPedestalRenderer::new);
 		
 		Yggdrasil.LOGGER.info("Render Registered! ");
 		
