354172e418de0352f115036cf03c9002af6f0d95
David <dm@lab4tech.ch>
2020-06-18 20:19:47 +0200

    Noise test

--- src/main/java/com/davidmontandon/yggdrasil/world/dimensions/svartalvheim/generator/SvartalvheimCavernBuilder.java
+++ src/main/java/com/davidmontandon/yggdrasil/world/dimensions/svartalvheim/generator/SvartalvheimCavernBuilder.java
@@ -30,7 +30,126 @@ public class SvartalvheimCavernBuilder extends SurfaceBuilder<SurfaceBuilderConf
 	   }
 
 	   public void buildSurface(Random random, IChunk chunkIn, Biome biomeIn, int x, int z, int startHeight, double noise, BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed, SurfaceBuilderConfig config) {
-	      int i = seaLevel + 1;
+
+			BlockState blockstate = LAYER1;
+			BlockState blockstate1 = LAYER1;
+			BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+		   
+			int i = seaLevel + 1;
+			int j = x & 15;
+			int k = z & 15;
+			int i1 = -1;
+		      
+		      boolean flag =  noise * (double)x * 0.03125D + (double)z * 0.03125D +  0.75D *  random.nextDouble() > 0.0D;
+		      boolean flag1 =  noise * (double)x * 0.03125D + (double)z * 0.03125D +  0.75D *  random.nextDouble() > 0.0D;
+		      int l = (int)(noise / 3.0D + 3.0D + random.nextDouble() * 0.25D);
+
+		      for(int j1 = HEIGHT; j1 >= 0; --j1) {
+		    	  
+		    	  
+			         blockpos$mutable.setPos(j, j1, k);
+			         BlockState blockstate2 = chunkIn.getBlockState(blockpos$mutable);
+			         if (blockstate2.getBlock() != null && !blockstate2.isAir()) {
+			            if (blockstate2.getBlock() == defaultBlock.getBlock()) {
+			               if (i1 == -1) {
+			                  if (l <= 0) {
+			                     blockstate = CAVE_AIR;
+			                     blockstate1 = LAYER1;
+			                  } else if (j1 >= i - 4 && j1 <= i + 1) {
+			                     blockstate = LAYER1;
+			                     blockstate1 = LAYER1;
+			                     if (flag1) {
+			                        blockstate = LAYER2;
+			                        blockstate1 = LAYER1;
+			                     }
+
+			                     if (flag) {
+			                        blockstate = LAYER3;
+			                        blockstate1 = LAYER3;
+			                     }
+			                  }
+
+			                  if (j1 < i && (blockstate == null || blockstate.isAir())) {
+			                     blockstate = defaultFluid;
+			                  }
+
+			                  i1 = l;
+			                  if (j1 >= i - 1) {
+			                     chunkIn.setBlockState(blockpos$mutable, blockstate, false);
+			                  } else {
+			                     chunkIn.setBlockState(blockpos$mutable, blockstate1, false);
+			                  }
+			               } else if (i1 > 0) {
+			                  --i1;
+			                  chunkIn.setBlockState(blockpos$mutable, blockstate1, false);
+			               }
+			            }
+			         } else {
+			            //i1 = -1;
+			         }
+		      }
+		      
+		      /*
+		      for(int j1 = HEIGHT; j1 >= 0; --j1) {
+		    	  
+		    	  
+			         blockpos$mutable.setPos(j, j1, k);
+			         BlockState blockstate2 = chunkIn.getBlockState(blockpos$mutable);
+			         if (blockstate2.getBlock() != null && !blockstate2.isAir()) {
+			            if (blockstate2.getBlock() == defaultBlock.getBlock()) {
+			               if (i1 == -1) {
+			                  if (l <= 0) {
+			                     blockstate = CAVE_AIR;
+			                     blockstate1 = LAYER1;
+			                  } else if (j1 >= i - 4 && j1 <= i + 1) {
+			                     blockstate = LAYER1;
+			                     blockstate1 = LAYER1;
+			                     if (flag1) {
+			                        blockstate = LAYER2;
+			                        blockstate1 = LAYER1;
+			                     }
+
+			                     if (flag) {
+			                        blockstate = LAYER3;
+			                        blockstate1 = LAYER3;
+			                     }
+			                  }
+
+			                  if (j1 < i && (blockstate == null || blockstate.isAir())) {
+			                     blockstate = defaultFluid;
+			                  }
+
+			                  i1 = l;
+			                  if (j1 >= i - 1) {
+			                     chunkIn.setBlockState(blockpos$mutable, blockstate, false);
+			                  } else {
+			                     chunkIn.setBlockState(blockpos$mutable, blockstate1, false);
+			                  }
+			               } else if (i1 > 0) {
+			                  --i1;
+			                  chunkIn.setBlockState(blockpos$mutable, blockstate1, false);
+			               }
+			            }
+			         } else {
+			            //i1 = -1;
+			         }
+		      }
+		      */
+		      
+		      /*
+		      for(int j1 = HEIGHT; j1 >= 0; --j1) {
+			         blockpos$mutable.setPos(j, j1, k);
+			         BlockState blockstate2 = chunkIn.getBlockState(blockpos$mutable);
+			         chunkIn.setBlockState(blockpos$mutable, blockstate, false);
+		      }
+		      */
+
+		      
+		      
+		   /*
+		  noise = 1 - noise ; 
+		   
+		  int i = seaLevel + 1;
 	      int j = x & 15;
 	      int k = z & 15;
 	      double d0 = 0.03125D;
@@ -84,7 +203,7 @@ public class SvartalvheimCavernBuilder extends SurfaceBuilder<SurfaceBuilderConf
 	            i1 = -1;
 	         }
 	      }
-
+		*/
 	   }
 
 	   public void setSeed(long seed) {
