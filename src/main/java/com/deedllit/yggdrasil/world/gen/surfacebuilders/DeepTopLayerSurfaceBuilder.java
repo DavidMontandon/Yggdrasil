a2887cbda6edacc9f247d43c8ef0dc607a50a156
David <dm@lab4tech.ch>
2020-08-10 16:34:07 +0200

    Island

--- src/main/java/com/deedllit/yggdrasil/world/gen/surfacebuilders/DeepTopLayerSurfaceBuilder.java
+++ src/main/java/com/deedllit/yggdrasil/world/gen/surfacebuilders/DeepTopLayerSurfaceBuilder.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.function.Function;
 
 import com.deedllit.yggdrasil.Yggdrasil;
+import com.deedllit.yggdrasil.init.BlockInit;
 import com.deedllit.yggdrasil.init.SurfaceBuilderInit;
 import com.mojang.datafixers.Dynamic;
 
@@ -33,111 +34,231 @@ public class DeepTopLayerSurfaceBuilder extends SurfaceBuilder<SurfaceBuilderCon
 		if (noise >= 0D) {
 
 			BlockState stone =  Blocks.STONE.getDefaultState() ; 
+			BlockState grass =  Blocks.GRASS_BLOCK.getDefaultState() ; 
+			BlockState whitesand =  BlockInit.WHITE_SAND.get().getDefaultState() ; 
+			BlockState top = config.getTop() ; 
+			BlockState middle = config.getUnder() ; 
+			BlockState bottom = config.getUnderWaterMaterial() ; 
 			
+						
+	        BlockState blockstate = top;
+	        BlockState blockstate1 = middle;
+	        BlockPos.Mutable blockpos$mutableblockpos = new BlockPos.Mutable();
+	        int i = -1;
+	        int k = x & 15;
+	        int l = z & 15;
+
+	        int sandLevelBottom = seaLevel -4 ;
+	        int sandLevelTop = seaLevel +1 ;
+	        int moutainLevelBottom = 105 ; 
+	        
+			double d = Biome.INFO_NOISE.noiseAt(x * 0.25D, z * 0.25D, false);		
+			if(d > 0) {
+				sandLevelTop += d * 1 ;
+				moutainLevelBottom += d * 5 ;
+			}
+
+	        int h ; 
 			int v = 2 ;
 			
 			if(noise <= 0.5D) {
 				maxH = 12  ; 
 				v = 1 ; 
-				//stone =  Blocks.STONE.getDefaultState() ; 						
 			} else if(noise <= 1.0D) {
 				maxH = 14  ; 
 				v = 1 ; 
-				//stone =  Blocks.STONE.getDefaultState() ; 			
 			} else if(noise <= 1.5D) {
 				maxH = 15  ; 
 				v = 2 ; 
-				//stone =  Blocks.STONE.getDefaultState() ; 			
 			} else if(noise <= 2.0D) {
 				maxH = 17  ; 
 				v = 2 ; 
-				//stone =  Blocks.STONE.getDefaultState() ; 
 			} else if(noise <= 2.6D) {
 				maxH = 18 ; 
 				v = 2 ; 
-				//stone =  Blocks.STONE.getDefaultState() ; 
 			} else if(noise <= 3.2D) {
 				maxH = 24  ; 
 				v = 4 ; 
-				//stone =  Blocks.STONE.getDefaultState() ;
 			} else if(noise <= 3.5D) {
 				maxH = 38  ; 
 				v = 5 ; 
-				//stone =  Blocks.STONE.getDefaultState() ; 
+			} else if(noise <= 3.8D) {
+				maxH = 40  ; 
+				v = 5 ; 
 			} else {
-				maxH = 41  ; 
-				//stone =  Blocks.STONE.getDefaultState() ; 					
+				maxH = 48  ; 
+				v = 2 ; 
 			}
 
 			maxH += random.nextInt(v);
 			
 			
-			BlockState top = config.getTop() ; 
-			BlockState middle = config.getUnder() ; 
-			BlockState bottom = config.getUnderWaterMaterial() ; 
 			
-	        BlockState blockstate = top;
-	        BlockState blockstate1 = middle;
-	        //BlockState stone =  Blocks.STONE.getDefaultState() ; 
-	        BlockPos.Mutable blockpos$mutableblockpos = new BlockPos.Mutable();
-	        int i = -1;
-	        //int j = (int)(noise / 3.0D + 3.0D + random.nextDouble() * 0.25D);
-	        //boolean flag = Math.cos(noise / 3.0D * Math.PI) > 0.0D;        
-	        int k = x & 15;
-	        int l = z & 15;
-
-	        int h ; 
+			
 	        
 	        for(int i1 = 5; i1 <= seaLevel; i1++) {
 	            blockpos$mutableblockpos.setPos(k, i1, l);            
 	            BlockState blockstateCur = chunkIn.getBlockState(blockpos$mutableblockpos);
 	            BlockState blockstateDown = chunkIn.getBlockState(blockpos$mutableblockpos.down());
-	                        
-	        	//if(j > 1.7 && flag) {	
+	            boolean setTop = true ;
+	            boolean setBottom = true ;
 	            
-	        		//Yggdrasil.LOGGER.info(blockstateCur.toString());
-	        		
-	        		
 	        		
 	        		if (blockstateCur == Blocks.WATER.getDefaultState() || blockstateCur == Blocks.AIR.getDefaultState()) {
-
+	        			
 	        			maxH = Math.max(maxH, i1) ; 
 
-	        			/*
-	        			h = random.nextInt(2) ; 
-	        			h = maxH + h ; 
-	        			for(int i2 = i1 ; i2 < h ; i2++) {
-	        	            blockpos$mutableblockpos.setPos(k, i2, l);            
-	                		chunkIn.setBlockState(blockpos$mutableblockpos, stone, false);        				
-	        				
-	        			}
-	        			
-	        			*/
 	        			h = maxH ;
+
+	        			
+
+	        			
+	        			int fhx = i1 + h ; 
 	        			
 	        			for(int i2 = 0 ; i2 <= h ; i2++) {
 	        				
 	        				int hx = i2 + i1;
+	        					        				
+	        				blockstate = stone ; 
+	        				
+	        				if(i2 == h) {
+	        					if (hx >= sandLevelBottom && hx <= sandLevelTop) {
+	        						blockstate = whitesand ;	        						
+	        					} else if (hx > sandLevelTop && hx < moutainLevelBottom){
+	        						blockstate = grass ; 
+	        					}
+	        				} else {
+	        					if (fhx >= sandLevelBottom && fhx <= sandLevelTop && hx >= sandLevelBottom) {
+	        						blockstate = whitesand ;	        							        						
+	        					}
+	        				}
 	        				
 	        	            blockpos$mutableblockpos.setPos(k, i2 + i1, l);    
-		                	chunkIn.setBlockState(blockpos$mutableblockpos, stone, false);        				
+		                	chunkIn.setBlockState(blockpos$mutableblockpos, blockstate, false);        				
 	        				
+		                	if(hx < seaLevel) {
+		        				if(setBottom && random.nextInt(15) == 0) {
+		        					if(k-1 >= 0) {
+		        						int nx = random.nextInt(h-6) - i2 ; 
+		        						if(nx > 0)
+		        							addExtraColumn(chunkIn, k-1, hx - random.nextInt(5), l, Blocks.DARK_OAK_LOG.getDefaultState(), Blocks.DARK_OAK_LOG.getDefaultState(), random.nextInt(nx), moutainLevelBottom) ;         		        								        						
+		        						
+		        					}
+		        					setBottom = false ; 
+		        				}
+		                	} else {
+		        				if(setTop && random.nextInt(15) == 0) {
+		        					if(k-1 >= 0) {
+		        						int nx = random.nextInt(h-6) - i2 ; 
+		        						if(nx > 0)
+		        							addExtraColumn(chunkIn, k-1, hx - random.nextInt(5), l, Blocks.ACACIA_PLANKS.getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState(), random.nextInt(nx), moutainLevelBottom) ;         		        								        						
+		        					}
+		        					setTop = false ; 
+		        				}
+		                	}
+
+		        			/*
+		        			//Add top
+		                	if(hx < seaLevel) {
+		        				if(setBottom && random.nextInt(30) == 0) {
+		    	        			//other direction block ?
+		        					
+		        					if(k-1 >= 0) {
+		        						int nx = random.nextInt(h-6-i2) ; 
+		        						if(nx > 0)
+		        							addExtraColumn(chunkIn, k-1, hx + random.nextInt(5) + 1, l, Blocks.DARK_OAK_LOG.getDefaultState(), Blocks.DARK_OAK_LOG.getDefaultState(), random.nextInt(h-6-i2), moutainLevelBottom) ;         		        						
+		        					}
+		    	        			setBottom = false ; 
+		        				}		                		
+		                	} else {
+			        			//Add bottom
+		        				if(setTop && random.nextInt(30)== 0) {
+		    	        			//other direction block ?
+		        					if(k-1 >= 0) {
+		        						int nx = random.nextInt(h-6-i2) ; 
+		        						if(nx > 0)
+		        							addExtraColumn(chunkIn, k-1, hx - random.nextInt(5), l, Blocks.ACACIA_PLANKS.getDefaultState(), Blocks.ACACIA_PLANKS.getDefaultState(), nx, moutainLevelBottom) ;         							        						
+		        					}
+		    	        			setTop = false ; 
+		        				}		                		
+		                	}
+		                	*/
+		        				
+
 	        			}
+	        			
+
+
 	        			
 	        			break ; 
 	            	}
 	            //}
 	        }
 		}
-		/*else {
+		else {
 			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock, defaultFluid, seaLevel, seed, config);			
 		}
-		*/
 		
-		SurfaceBuilderInit.TROPICAL.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock, defaultFluid, seaLevel, seed, SurfaceBuilder.STONE_STONE_GRAVEL_CONFIG);
+		
+		//SurfaceBuilderInit.TROPICAL.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock, defaultFluid, seaLevel, seed, SurfaceBuilder.STONE_STONE_GRAVEL_CONFIG);
 
 		
 
 	}
+	
+	
+	private void addExtraColumn(IChunk chunkIn, int x, int y, int z, BlockState stone, BlockState grass, int n, int moutainLevelBottom) {
+		
+		if(canAdd(chunkIn, x, y, z, n) == false)
+			return ; 
+		
+        BlockPos.Mutable blockpos$mutableblockpos = new BlockPos.Mutable();
+
+		
+		for(int y2 = 0 ; y2 <= n ; y2++) {
+	        int y3 = y2+y ; 
+
+	        blockpos$mutableblockpos.setPos(x, y3, z);   
+	        
+	        
+	        if(y2 == n) {
+	        	if(y3 < moutainLevelBottom) {
+	        		chunkIn.setBlockState(blockpos$mutableblockpos, grass, false);
+	        	} else { 
+	        		chunkIn.setBlockState(blockpos$mutableblockpos, stone, false);	
+	        	}
+	        } else {
+	        	chunkIn.setBlockState(blockpos$mutableblockpos, stone, false);        					        	
+	        }
+	        
+	        
+		}
+	}
+	
+	private boolean canAdd(IChunk chunkIn, int x, int y, int z, int n) {
+        BlockPos.Mutable blockpos$mutableblockpos = new BlockPos.Mutable();
+        blockpos$mutableblockpos.setPos(x, y, z);   
+        BlockState blockstate = chunkIn.getBlockState(blockpos$mutableblockpos.down());
+
+        if (blockstate != Blocks.AIR.getDefaultState()) {
+        	return false ;
+        }
+
+        blockstate = chunkIn.getBlockState(blockpos$mutableblockpos);
+        
+        for(int y2 = 1 ; y2 <= n ; y2++) {
+            
+	        if (blockstate != Blocks.AIR.getDefaultState()) {
+	        	return false ;
+	        }
+            
+	        blockpos$mutableblockpos.setPos(x, y+y2, z);   
+            blockstate = chunkIn.getBlockState(blockpos$mutableblockpos);
+		}
+		
+		return true ; 
+	}
+	
+	
 
 }
